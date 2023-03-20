2ade95e10e5084a6568a343ecf572711d1854fea
kingcatttttt <ssvyat7007@gmail.com>
2023-03-20 19:20:53 +0100

    lesson4

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
-function a1() {
-    console.log('work a1');
-    f2();
+let a = [1,2,3,4];
+let sum = 0;
+
+for (let i = 0; i < a.length; i++) {
+    sum = sum + a[i];
+}
+console.log(sum);
+
+let b = a.reduce( (prev, item, index) => {
+        return prev+item;
+},0);
+
+console.log(b);
+
+a = [3,4,5,1,6,6];
+let max = 0;
+for (let i = 0; i < a.length; i++) {
+    if (a[i] > max) {
+        max = a[i];
+    }
 }
 
-function f2() {
-    console.log('f2 working');
-    console.trace();
-}
-a1();
+console.log(max)
 
-const c ={
-    one : 1,
-    two : 2,
-    three : 3,
-}
+let d = a.reduce((prev,item) => {
+    if(prev < item) {
+        return item;
+    } else {
+        return prev;
+    }
+    if (item > prev[1]) {
+        return [index, item];
+    } else {
+        return prev;
+    }
+})
 
-let p = document.querySelector('p')
-let div = document.querySelector('div');
-console.log(div);
-console.log(p);
-console.dir(p);
+// console.log(c);
 
-console.dir(one1);
-console.dir(document.myForm)
-console.log(window.myForm);
+// let d = a.reduce((prev,item, index) => {
+//     if(prev < item) {
+//         return item;
+//     } else {
+//         return prev;
+//     }
+//     if (item > prev[1]) {
+//         return [index, item];
+//     } else {
+//         return prev;
+//     }
+// }, a[0,a[o]])
+
+// console.log(d);
+
+const data = [[1,2,3],[11,2,3],[5,6,7]]
+
+const flat = data.reduce( (prev, item) => {
+    return prev.concat(item);
+})
+
+console.log(flat)
