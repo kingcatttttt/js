93091f413fc5389d36a574b9fb40b3c4189fce68
kingcatttttt <ssvyat7007@gmail.com>
2023-03-21 21:29:04 +0100

    lesson6

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const filterBox = document.querySelectorAll('.box')
+const animals = ['1','2','3'];
 
-document.querySelector('nav').addEventListener('click', event => {
-    if (event.target.tagName !== 'LI') return false;
-    
-    let filterClass = event.target.dataset['f'];
-   
-    filterBox.forEach( elem => {
-        elem.classList.remove('hide');
-        if (!elem.classList.contains(filterClass) && filterClass!== 'all'){
-            elem.classList.add('hide');
-        }
-    });
-});
+function ShowArr(arr, elem) {
+    let out = '';
+    for (let i=0; i < arr.lenght; i++) {
+        // if (arr[i] !== undefined) {
+            
+        // }
+        out += '<div><h2><h2><span>${i}</span></div>'
+        
+    }
+    document.querySelector(elem).innerHTML = out;
+
+document.querySelector('.out-1-source').innerHTML = 'const numbers=['+animals+']';
+document.querySelector('.out-1-source-lenght').innerHTML = 'lenght const' + animals.length;
+
+}
+
+ShowArr(animals, '.out-1-source-image')
+
+
+function addToArray() {
+    if (!Number.isInteger(+document.querySelector('.array-index').value)) {
+        alert ('только полние');
+        return false;
+    };
+    let index = +document.querySelector('.array-index').value;
+    if (index > 50) {
+        alert("delete this")
+        return false;
+    } else if (index < 0){
+        alert('index biggest 0');
+        return false;
+    }
+    animals[index] = document.querySelector('.array-element').value;
+    ShowArr(animals, '.out-1-source-image')
+}
+document.querySelector('.add-to-array').onclick = addToArray;
