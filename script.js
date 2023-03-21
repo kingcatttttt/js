dd32ba79e752bba1d9afb70abf03fc29d2494903
kingcatttttt <ssvyat7007@gmail.com>
2023-03-21 20:41:48 +0100

    lesson5

--- script.js
+++ script.js
@@ -1,61 +1,14 @@
-let a = [1,2,3,4];
-let sum = 0;
+const filterBox = document.querySelectorAll('.box')
 
-for (let i = 0; i < a.length; i++) {
-    sum = sum + a[i];
-}
-console.log(sum);
-
-let b = a.reduce( (prev, item, index) => {
-        return prev+item;
-},0);
-
-console.log(b);
-
-a = [3,4,5,1,6,6];
-let max = 0;
-for (let i = 0; i < a.length; i++) {
-    if (a[i] > max) {
-        max = a[i];
-    }
-}
-
-console.log(max)
-
-let d = a.reduce((prev,item) => {
-    if(prev < item) {
-        return item;
-    } else {
-        return prev;
-    }
-    if (item > prev[1]) {
-        return [index, item];
-    } else {
-        return prev;
-    }
-})
-
-// console.log(c);
-
-// let d = a.reduce((prev,item, index) => {
-//     if(prev < item) {
-//         return item;
-//     } else {
-//         return prev;
-//     }
-//     if (item > prev[1]) {
-//         return [index, item];
-//     } else {
-//         return prev;
-//     }
-// }, a[0,a[o]])
-
-// console.log(d);
-
-const data = [[1,2,3],[11,2,3],[5,6,7]]
-
-const flat = data.reduce( (prev, item) => {
-    return prev.concat(item);
-})
-
-console.log(flat)
+document.querySelector('nav').addEventListener('click', event => {
+    if (event.target.tagName !== 'LI') return false;
+    
+    let filterClass = event.target.dataset['f'];
+   
+    filterBox.forEach( elem => {
+        elem.classList.remove('hide');
+        if (!elem.classList.contains(filterClass) && filterClass!== 'all'){
+            elem.classList.add('hide');
+        }
+    });
+});
