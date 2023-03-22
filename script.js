d8e53332930c4105d202016531d15b09e068654a
kingcatttttt <ssvyat7007@gmail.com>
2023-03-22 20:29:49 +0100

    lesson11

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-let a = [1,2,3,[6],[8]];
-// let b = a;
-// let b = [...a]
+function a(b) {
+        return b * 5
+}
 
-// for (let i = 0; i <a.length; i++ ) {
-//     b[i] = a[i];
-// }
-// let b = JSON.parse(JSON.stringify(a));
-// let b = a.concat([]);
-let b = Array.from(a);
-
-
-
-a[3].push(999);
-a.push("test")
-
-console.log("array a")
-console.log(a)
-
-console.log("array b")
-console.log(b)
+console.log(a(5))
