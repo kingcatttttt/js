8f8c165b4b0180bc8d8b1a90cf4a15f8fa3c73f9
kingcatttttt <ssvyat7007@gmail.com>
2023-03-22 20:45:15 +0100

    lesson12

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function a(b) {
-        return b * 5
-}
+const a = [11,22,33,44]
 
-console.log(a(5))
+console.log(a[2])
+
+const b = {g : 8, p : 223, w : 22}
+let c = "p"
+console.log(c[g])
+console.log(c"p")
