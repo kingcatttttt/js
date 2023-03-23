a34da4de4e6524b8dbc8b6d1e688c56b29520d66
kingcatttttt <ssvyat7007@gmail.com>
2023-03-23 22:21:21 +0100

    lesson15

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const z = n => n >= 0 ? n : 0;
-console.log(z(-5));
-console.log(z(35));
+const arr = [1,2,4,56,7,3,6,3,6,2,6,765,8,9]
+console.log(arr)
 
-const w = n => ({"temp" : n})
-console.log(w(5));
+//1
+
+// let a1 =[... new Set(arr)];
+// console.log(a1)
+
+//1.1
+
+// let a1 = Array.from(new Set(arr));
+// console.log(a1)
+
+//1.2
+
+// let a2 = arr.filter( (item, index) => {
+//     return arr.indexOf(item) === index;
+
+// })
+// console.log(a2);
+
+//1.3
+// let a2 = arr.filter( (item, index) => {
+//     return arr.indexOf(item) !== index;
+
+// })
+// console.log(a2);
+
+//1.4 
+let a3 =arr.reduce((result, item) => {
+    return result.includes(item) ? result : [...result, item];
+
+}, [])
+console.log(a3)
