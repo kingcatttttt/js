dae7748f94e458c31d00aca465e151bfd14bbc6e
kingcatttttt <ssvyat7007@gmail.com>
2023-03-24 18:19:31 +0100

    lesson18

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-let fontSize = 16;
-if (localStorage.getItem("font-size")) {
-    fontSize = localStorage.getItem("font-size")
-}
+// // // let dwayne = {};
+// // // let daniel = {
+// // //     firstName: "Dianiel"
+// // // }
 
-document.querySelector(".font-zoom_item").textContent = fontSize;
-document.body.style.fontSize = fontSize + "px";
+// // // let jason = {
+// // //     key: "jason"
+// // // }
+// // // document.querySelector(".our").innerHTML = daniel;
+// // // dwayne[daniel] = 123;
+// // // dwayne[jason] = 456;
 
-document.querySelector(".font-zoom_plus").onclick = () => {
-    fontSize++;
-    localStorage.setItem(".font-size", fontSize)
-    document.querySelector(".font-zoom_item").textContent = fontSize;
-    document.body.style.fontSize = fontSize + "px";
-}
-document.querySelector(".font-zoom_minus").onclick = () => {
-    fontSize--;
-    localStorage.setItem(".font-size", fontSize)
-    document.querySelector(".font-zoom_item").textContent = fontSize;
-    document.body.style.fontSize = fontSize + "px";
-}
+// // // dwayne[daniel];
 
+// // const func = (function (a) {
+// //     delete a;
+// //     return a;
+// // })(5);
+// // function func2 (A) {
+// //     return A;
+// // }
+// // let b = func2(5)
+// // console.log(b)
+// // console.log(func)
+
+// console.log({a : 1} == {a: 1});
+// console.log({a : 1} === {a: 1});
+
+const jamesBoand = {
+    firstName: "Daniel",
+    lastName: "craig",
+    getFullName: function() {
+        return '${this.firtsName} ${this.lastName}'.
+    }
+
+};
+
+console.log(jamesBoand.getFullName())
