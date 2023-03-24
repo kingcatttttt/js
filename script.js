34cf5cec02b19adf80fd2c599d99b3f0974262ea
kingcatttttt <ssvyat7007@gmail.com>
2023-03-24 19:33:03 +0100

    lesson20

--- script.js
+++ script.js
@@ -1,70 +1,44 @@
-// // // // // // // // // let n1 = 44.56;
-// // // // // // // // // console.log(Math.floor(n1))
+console.log("hello")
+console.log("%c hello" , "color: orange; background: red; font-weight:bold; text-transform: uppercase; padding: 10px; margin: 50px; width: 300px; border: 2px solid black;")
+console.group("text");
+console.groupEnd();
 
-// // // // // // // // let n2 = 3;
-// // // // // // // // console.log(Math.pow(n2,2));
-// // // // // // // // console.log(n2**3);
+function one() {
+    console.time()
+    let a = 5;
+    a++;
+    two();
+    console.timeLog();
+    console.timeEnd();
 
-// // // // // // // let n3 = "3";
-// // // // // // // console.log(parseInt(n3))
-// // // // // // // console.log(+n3)
+}
 
-// // // // // // let n4 = true 
-// // // // // // if (n4 === true) {
-// // // // // //     console.log("4")
-// // // // // // }
+function two() {
+    let b = 5;
+    b++;
+    console.trace("function 2");
+}
+one();
 
-// // // // // // n4 && console.log("4")
 
-// // // // // let n5 = 3;
-// // // // // console.log(n5.toString());
-// // // // // console.log(+n5);
+let obj = {
+    one : 4,
+    two : 5,
+    hello : "hi"
+}
+console.log(obj)
+console.table(obj)
 
-// // // // function f6_1() {
-// // // //     console.log("6_1")
-// // // // }
-// // // // function f6_2() {
-// // // //     console.log("6_2")
-// // // // }
+function four() {
+    console.count("hi")
+}
 
-// // // // let n6 = 7;
-// // // // if (n6 === 7) {
-// // // //     f6_1()
-// // // // }
-// // // // else {
-// // // //     f6_2()
-// // // // }
+four();
+four();
+four();
 
-// // // // (n6 === 7 ? f6_1 : f6_2)()
+console.assert()
 
-// // // function f7(param) {
-// // //     if (param === undefined) {
-// // //         throw new Error("problem")
-// // //     }
-// // //     return param;
-// // // }
-
-// // // console.log(f7(4));
-
-// // // let checkParam = () => {
-// // //     throw new Error("problem")
-// // // }
-
-// // // let f7_1 = (param = checkParam()) => {
-// // //     return param;
-// // // }
-// // // console.log(f7_1(444));
-
-// // let n8 = "sample string";
-// // console.log(n8.charAt(4))
-// // console.log(n8[4]);
-
-// let n9 = "hello 
-// h1
-// mahai
-// test";
-// console.log(n9);
-
-let n10 = 34;
-let result = n10 || false
-console.log(result);
+let container = document.querySelector("container")
+console.dir(container)
+console.dir(obj)
