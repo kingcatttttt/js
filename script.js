9e1f9927232085a67f76c536f452a93e9d885fe6
kingcatttttt <ssvyat7007@gmail.com>
2023-03-24 18:59:53 +0100

    lesson19

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-// // // let dwayne = {};
-// // // let daniel = {
-// // //     firstName: "Dianiel"
+// // // // // // // // // let n1 = 44.56;
+// // // // // // // // // console.log(Math.floor(n1))
+
+// // // // // // // // let n2 = 3;
+// // // // // // // // console.log(Math.pow(n2,2));
+// // // // // // // // console.log(n2**3);
+
+// // // // // // // let n3 = "3";
+// // // // // // // console.log(parseInt(n3))
+// // // // // // // console.log(+n3)
+
+// // // // // // let n4 = true 
+// // // // // // if (n4 === true) {
+// // // // // //     console.log("4")
+// // // // // // }
+
+// // // // // // n4 && console.log("4")
+
+// // // // // let n5 = 3;
+// // // // // console.log(n5.toString());
+// // // // // console.log(+n5);
+
+// // // // function f6_1() {
+// // // //     console.log("6_1")
+// // // // }
+// // // // function f6_2() {
+// // // //     console.log("6_2")
+// // // // }
+
+// // // // let n6 = 7;
+// // // // if (n6 === 7) {
+// // // //     f6_1()
+// // // // }
+// // // // else {
+// // // //     f6_2()
+// // // // }
+
+// // // // (n6 === 7 ? f6_1 : f6_2)()
+
+// // // function f7(param) {
+// // //     if (param === undefined) {
+// // //         throw new Error("problem")
+// // //     }
+// // //     return param;
 // // // }
 
-// // // let jason = {
-// // //     key: "jason"
+// // // console.log(f7(4));
+
+// // // let checkParam = () => {
+// // //     throw new Error("problem")
 // // // }
-// // // document.querySelector(".our").innerHTML = daniel;
-// // // dwayne[daniel] = 123;
-// // // dwayne[jason] = 456;
 
-// // // dwayne[daniel];
+// // // let f7_1 = (param = checkParam()) => {
+// // //     return param;
+// // // }
+// // // console.log(f7_1(444));
 
-// // const func = (function (a) {
-// //     delete a;
-// //     return a;
-// // })(5);
-// // function func2 (A) {
-// //     return A;
-// // }
-// // let b = func2(5)
-// // console.log(b)
-// // console.log(func)
+// // let n8 = "sample string";
+// // console.log(n8.charAt(4))
+// // console.log(n8[4]);
 
-// console.log({a : 1} == {a: 1});
-// console.log({a : 1} === {a: 1});
+// let n9 = "hello 
+// h1
+// mahai
+// test";
+// console.log(n9);
 
-const jamesBoand = {
-    firstName: "Daniel",
-    lastName: "craig",
-    getFullName: function() {
-        return '${this.firtsName} ${this.lastName}'.
-    }
-
-};
-
-console.log(jamesBoand.getFullName())
+let n10 = 34;
+let result = n10 || false
+console.log(result);
