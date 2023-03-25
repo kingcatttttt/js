c3e8767bd0d39e52d33a709cad21a8ffaafe2f4f
kingcatttttt <ssvyat7007@gmail.com>
2023-03-25 20:23:56 +0100

    lesson23

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let block = document.createElement("div")
-block.classList.add("block")
-document.querySelector(".box-1").append(block);
+const zone1 = document.querySelector(".zone-1");
+const zone2 = document.querySelector(".zone-2");
+const ufo = document.querySelector("#ufo");
+
+zone1.ondragover = allowDrop;
+zone2.ondragover = allowDrop;
+function allowDrop(event) {
+    event.preventDefault;
+}
+
+ufo.ondragstart = drag;
+function drag(event) {
+    event.dataTransfer.setData("id", event.target.id);
+
+}
+
+zone1.ondrop = drop;
+zone2.ondrop = drop;
+function drop(event) {
+    let itemId = event.dataTransfer.getData("id");
+    console.log(itemId);
+    event.target.append(document.getElementById(itemId));
+}
