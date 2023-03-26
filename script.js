477ed69257f12977dc53838565103afe36f0d876
kingcatttttt <ssvyat7007@gmail.com>
2023-03-26 18:43:16 +0200

    lesson 24

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-const romes = {"m": 1000,"cm":900,"d":500,"cd":400,"c":100,"xc":90,"l":50,"xl":40,"x":10,"ix":9,"v":5,"IV":4,"I":1,};
+// document.querySelector(".btn-2").addEventListener("click", check);
+// document.querySelector(".btn-2").addEventListener("click", toggleScreen);
 
-// function romanConvertOne(a) {
-//     let resuslt = "";
-//     decimalValue.map( (item,index) => {
-//         while (item <= a) {
-//             resuslt += romanValue[index];
-//             a = a - item;
-//             console.table({
-//                 "a": a + item,
-//                 "item": item,
-//                 "a = a - item" : a,
-//                 "result" : resuslt,
-//             })
-           
-//         }
-//         return resuslt;
-//     })
+// function check() {
+//     console.log("full screen")
+//     console.log(document.fullscreenEnabled);
+
+//     console.log("fullscreenelemnt")
+//     console.dir(document.fullscreenElement);
 // }
-function romanConvertOne(a) {
-    let result = "";
-    for (let key in romes) {
-        while (romes[key] <= a) {
-            result += key;
-            a = a - romes[key];
+
+// function toggleScreen() {
+//     if (!document.fullscreenElement) {
+//         document.documentElement.requestFullscreen();
+//     } else {
+//         if (document.fullscreenEnabled) {
+//             document.exitFullscreen();
+//         }
+//     }
+// }
+
+const img = document.querySelector(".img-1");
+
+img.addEventListener("click", toggleScreen);
+
+function toggleScreen() {
+    if (!document.fullscreenElement) {
+        img.requestFullscreen();
+    } 
+    else {
+        if (document.fullscreenEnabled) {
+            document.exitFullscreen();
         }
     }
+
 }
-// document.querySelector("button").onclick = () => {
-//     let num = +document.querySelector("input").value;
-//     document.querySelector("out").innerHTML = romanConvertOne(num);
-// }
-console.log(romanConvertOne(a))
+
+document.documentElement.addEventListener("fullscreenchanger", function() {
+   console.log("test")
+})
