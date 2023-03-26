8a9a59ba26dc8b9d3a03d1d4bc138a6ac3b892e1
kingcatttttt <ssvyat7007@gmail.com>
2023-03-26 18:54:26 +0200

    lesson 25

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
-// document.querySelector(".btn-2").addEventListener("click", check);
-// document.querySelector(".btn-2").addEventListener("click", toggleScreen);
-
-// function check() {
-//     console.log("full screen")
-//     console.log(document.fullscreenEnabled);
-
-//     console.log("fullscreenelemnt")
-//     console.dir(document.fullscreenElement);
-// }
-
-// function toggleScreen() {
-//     if (!document.fullscreenElement) {
-//         document.documentElement.requestFullscreen();
-//     } else {
-//         if (document.fullscreenEnabled) {
-//             document.exitFullscreen();
-//         }
-//     }
-// }
-
-const img = document.querySelector(".img-1");
-
-img.addEventListener("click", toggleScreen);
-
-function toggleScreen() {
-    if (!document.fullscreenElement) {
-        img.requestFullscreen();
-    } 
-    else {
-        if (document.fullscreenEnabled) {
-            document.exitFullscreen();
-        }
-    }
-
+const iframe = document.querySelector("iframe,div.plaer");
+for (let i = 0; i < iframe.lenght; i++) {
+    iframe[i].remove();
 }
 
-document.documentElement.addEventListener("fullscreenchanger", function() {
-   console.log("test")
-})
+document.onclick = removeNode;
+
+function removeNode(event) {
+    event.preventDefeault();
+    event.target.remove();
+    return false;
+}
