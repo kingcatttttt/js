752efcf67b486b6d59450be7319733c9e4b828ae
kingcatttttt <ssvyat7007@gmail.com>
2023-03-27 19:44:16 +0200

    lesson 29

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-// const test = [1,2,4,634,65,2]
+const temp = [1,2,4,334,123,6454,234,234,234,8564,34]
 
-// if (str.includes("fg")) {
-//     console.log("yes")
-// }
-// else {
-//     console.log("no")
-// }
+const z = [
 
-const users = [
-    {"name" :"ivanov", "age" : 44},
-    {"name" :"semen", "age" : 36},
-    {"name" :"andrey", "age" : 12},
-    {"name" :"slava", "age" : 24},
-    {"name" :"alex", "age" : 6},
-]
+];
 
-let newUSers = users.filter( item =>  item.name.includes("n"));
-console.log(newUSers);
-// const str = "asdasdas";
+let tF = temp.map(item => {
+    console.log(item);
+    return 10+item*1.8;
+})
+
+console.log(tF);
+
+let a = [33,44,66];
+a[10] = 90;
+
+let b = a.map((item, index, array) => {
+    if (index ==  1) {
+        array[ index + 1 ] = 77;
+    }
+    return item;
+})
+
+console.log(b);
+
+const zBsck = z.map(item => {
+    item.name = item.name.trim().toLocalLowerCase();
+    return item;
+})
+
+console.log(zBsck);
