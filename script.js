febea360c0b7ac36bb7eba50d0ca9786c170e52c
kingcatttttt <ssvyat7007@gmail.com>
2023-03-28 21:22:02 +0200

    lesson 33

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const a = [
-    {"id":55 , "cty": "arc"},
-    {"id":44 , "cty": "asd"},
-    {"id":33 , "cty": "rcs"},
-];
+let a = [3,3,4,5,23,44]
 
-let b = a.reduce( (acum,item) => {
-    acum.push(item.id);
-    return acum;
-})
+let b = a.shift();
 console.log(b);
-console.log(Array.isArray(b))
+console.log(a);
+
+let c = a.unshift(23);
+console.log(c)
+
+let d = [22,44,435345,62432,323463,43556,]
+
+let f = d.slice(3,4);
+console.log(f);
+console.log(d);
+
+let e = "hello";
+
+e.shift();
+console.log(e.slice(1,4))
