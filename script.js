61725448bbc61f8038e0735c33a32fedc4f117ff
kingcatttttt <ssvyat7007@gmail.com>
2023-04-01 03:59:46 +0200

    lesson46

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-const browser = [
+const clienst = [
 
 ]
-
-let a = [99,88]
-console.log(a)
-
-a[0] = 55;
-console.log(a)
-a[a.length] = 55;
-console.log(a)
-a.push(44);
-console.log(a)
-console.log(a.push(8))
-console.log(a)
-
-function hello() {
- a.push(b);
-}
-
-a.pop();
-console.log(a)
-console.log(a.pop())
-console.log(a)
-const b = [3]
+const a = [43,31,23,2,1]
+let b = a.filter( item => {
+    return true;
+})
 console.log(b)
-console.log(b.pop())
-
-const cars = [];
-document.querySelector(".puhs").onclick = () => {
-    let id = document.querySelector(".goods").value;
-    if (!cars.includes(id)) {
-        cars.push(id)
-    }
-    console.log(cars)
-}
