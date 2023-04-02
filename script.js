4b5785beb262c046b07ac7a2cbbc24145d871ac6
kingcatttttt <ssvyat7007@gmail.com>
2023-04-02 21:21:04 +0200

    lesson51

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let a = [3,4,5]
-let b = [6,7,8]
+const a = [3,4,5,6,7,8]
+const c = [
+    {name: "ivan", age : 12},
+    {name: "semen", age : 15},
+    {name: "bob", age : 32},
+    {name: "a", age : 23},
+]
 
-const c = [...a, ...b];
-console.log(c)
+let b = e.some(item => {
+             return true;       
 
-const d = [...b]
-console.log(d);
+    
+})
+console.log(b);
 
-const i = [..."hello"]
-console.log(i)
-
-
-const e = [...new Set([1,2,3,4,5,6,7])]
-console.log(e)
-
-function test() {
-    console.log([...arguments]);
-}
-
-test(1,2,3,4)
-
-let p = document.querySelectorAll("p");
-console.dir(p);
+let e = [];
