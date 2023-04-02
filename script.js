b297e45f59e2b67a92d5ad6b4e3573a7441d54b6
kingcatttttt <ssvyat7007@gmail.com>
2023-04-02 22:10:15 +0200

    lesson54

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let str = "";
-let a = str.split("")
-console.log(a)
+let a = [3,4, [6,7], ,5,6];
+let b = a.flat(2)
+console.log(b)
+console.log(a);
 
-let b = ["h","e","l","l","o"]
+let c = [1,2,2,1,1,1]
+let g = c.fill(2,0,4)
 
-let c = b.join("");
+console.log(g)
 
-console.log(c)
-
+let s = [1,2,3,4,5,6,7,8]
+console.log(Object.keys(s));
