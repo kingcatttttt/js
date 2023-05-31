da44e30e3f00b683c688baf05936fe399c679e08
kingcatttttt <ssvyat7007gmail.com>
2023-05-31 14:41:14 +0200

    test

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-let our = document.querySelector(".our")
-let arr = []
-const i1 = document.querySelector(".i1").addEventListener("change", () => {
-    let value = document.querySelector(".i1").value;
-    arr.push(value)
-    console.log(arr)
-    let result = arr.reduce((a,b) => (+a +  +b) / arr.length )
-    console.log(result)
-    document.querySelector(".i1").value = "";
-    our.innerHTML = `${arr} = ${result}`
-})
+let a = "текст"
+let b = "и пошла вода"
+const truncate = (text, length) => {
+  // BEGIN (write your solution here)
+  let textt = a.slice(0,3)
+  let tex = b.slice(0,5)
+  let points = textt + "..."
+  let bb = tex +  "..."
+  return console.log(`${points}\n${bb}`)
+  // END
+};
+truncate()
 
-const b1 = document.querySelector(".b1").addEventListener("click", () => {
-    arr.shift()
-    console.log(arr)
-    let result = arr.reduce((a,b) => (+a +  +b) / arr.length )
-    console.log(result)
-    document.querySelector(".i1").value = "";
-    our.innerHTML = `${arr} = ${result} `
-})
-const b2 = document.querySelector(".b2").addEventListener("click", () => {
-    arr = []
-    our.innerHTML = arr;
-})
+
+
